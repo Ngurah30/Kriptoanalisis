307c3889618350dc5bba0dc9dee086931af31b2a
Ngurah30 <71589329+Ngurah30@users.noreply.github.com>
2021-03-01 22:04:49 +0800

    Prime Number Checker1.py

---
+++ Tugas 1/Prime Number Checker/Prime Number Checker1.py
@@ -0,0 +1,13 @@
+# Menginputin bilangan yang akan di cek
+bil = int(input("Masukkan bilangan : "))
+# Bilangan prima harus lebih besar dari 1
+if bil > 1:
+    for i in range(2, bil):
+        if (bil % i) == 0:
+            print(bil, "bukan bilangan prima")
+            break
+    else:
+        print(bil,"adalah bilangan prima")
+# Jika bilangan kurang atau sama dengan satu
+else:
+    print(bil, "bukan bilangan prima")
